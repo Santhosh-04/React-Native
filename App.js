5030371284e6e2178d168f7eacc2efe73de11ef4
Santhosh-04 <112604595+Santhosh-04@users.noreply.github.com>
2023-01-03 22:29:57 +0530

    Update App.js

--- App.js
+++ App.js
@@ -1,8 +1,7 @@
 import call from 'react-native-phone-call';
-
+import React,{useEffect, useState} from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
-import React, { useState } from 'react';
 import {
   SafeAreaView,
   StyleSheet,
@@ -11,7 +10,8 @@ import {
   TouchableOpacity,
   TextInput,
   Button,
-  marginTop
+   FlatList,
+  TouchableHighlight
 } from 'react-native';
 const Stack = createNativeStackNavigator();
 
@@ -35,6 +35,7 @@ const MyStack = () => {
           options={{title: 'Add Contact'}}
         />
         <Stack.Screen name="Profile" component={ProfileScreen} />
+          <Stack.Screen name="ViewForm" component={ListPage} />
       </Stack.Navigator>
     </NavigationContainer>
   );
@@ -59,17 +60,20 @@ const DialScreen = ({navigation}) => {
   };
   return (
     <SafeAreaView style={styles.container}>
+    <View>
      <Button
       title="Go to Contacts"
       onPress={() =>
         navigation.navigate('Home')
       }
     />
+    <Button onPress={()=>navigation.navigate('ViewForm')} title="View Form" color="#345584" />
     <Button title="ADD CONTACT"
      onPress={() =>
         navigation.navigate('Login')
      }
     />
+    </View>
       <View style={styles.container}>
         <Text style={styles.titleText}>
           Make a Phone Call
@@ -100,6 +104,132 @@ const DialScreen = ({navigation}) => {
 
   );
 };
+
+const Formpage = ({navigation}) => {
+  const [Form,setForm ] = useState({name:"",email:"",mobile:"",team:"",city:""})
+  const formChange = (value,field) =>{
+    let prev = {...Form}
+    prev[field] = value;
+    setForm(prev)
+  }
+  const handleSubmit = async() => {
+    console.log(Form);
+    const response = await fetch('http://192.168.9.6:3001/api/insert', {
+      method: 'POST',
+      mode: 'cors', 
+      cache: 'no-cache', 
+      credentials: 'same-origin', 
+      headers: {
+        'Content-Type': 'application/json'
+       
+      },
+     
+      referrerPolicy: 'no-referrer',
+      body: JSON.stringify(Form) 
+    });
+    console.log(response.json());
+  };
+
+  return (
+    <SafeAreaView>
+      <TextInput
+        onChangeText={(text) =>formChange(text,"name")}
+        value={Form.name}
+        style={styles.input}
+        placeholder="Enter your name"
+      />
+      <TextInput
+        onChangeText={(text) =>formChange(text,"mobile")}
+        value={Form.mobile}
+        style={styles.input}
+        placeholder="Enter Phone"
+      />
+      <TextInput
+        onChangeText={(text) =>formChange(text,"email")}
+        value={Form.email}
+        style={styles.Input}
+        placeholder="Enter Email"
+      />
+      <TextInput
+        onChangeText={(text) =>formChange(text,"team")}
+        value={Form.team}
+        style={styles.Input}
+        placeholder="My Team"
+      />
+      <TextInput
+       onChangeText={(text) =>formChange(text,"city")}
+        value={Form.city}
+        style={styles.Input}
+        placeholder="City"
+      />
+      <TouchableHighlight
+        onPress={()=>{
+          handleSubmit()
+          Alert.alert('Form Submitted')
+        }}
+        style={styles.button}>
+        <Text style={styles.buttonText}>Submit</Text>
+      </TouchableHighlight>
+      <Button onPress={()=>navigation.navigate('Dial')} title="home" color="#841584" />
+    </SafeAreaView>
+  );
+};
+
+const ListPage = ({navigation}) => {
+  const [Form,setForm ] = useState([])
+  const [selectedId, setSelectedId] = useState(null);
+  const handleSubmits = async() => {
+    fetch('http://192.168.9.6:3001/api/find')
+    .then((response) => response.json())
+    .then((data) => setForm(data.data));
+  }
+ useEffect(()=>{
+  handleSubmits()
+ },[])
+ const Item = ({ item, onPress, backgroundColor, textColor }) => (
+  <TouchableOpacity onPress={onPress} style={[styles.item, backgroundColor]}>
+    <Text style={[styles.title, textColor]}> Name  {item?.name}</Text>
+  
+    <Text style={[styles.title, textColor]}>email  {item?.email}</Text>
+    <Text style={[styles.title, textColor]}>mobile  {item?.mobile}</Text>
+    <Text style={[styles.title, textColor]}>my team  {item?.team}</Text>
+    <Text style={[styles.title, textColor]}>city  {item?.city}</Text>
+  </TouchableOpacity>
+);
+ const renderItem = ({ item }) => {
+  const backgroundColor = item.id === selectedId ? "#6e3b6e" : "#f9c2ff";
+  const color = item.id === selectedId ? 'white' : 'black';
+
+  return (
+<>
+    <Item
+      item={item}
+      onPress={() => setSelectedId(item.id)}
+      backgroundColor={{ backgroundColor }}
+      textColor={{ color }}
+    />
+    <View style={{marginBottom:21}} />
+    </>
+  );
+};
+  return (
+    <SafeAreaView>
+      <FlatList
+        data={Form}
+        renderItem={renderItem}
+        keyExtractor={(item) => item._id}
+       
+      />
+      <TouchableHighlight
+       
+        style={styles.button}>
+        <Text style={styles.buttonText}>Submit</Text>
+      </TouchableHighlight>
+      <Button onPress={()=>navigation.navigate('Home')} title="home" color="#841584" />
+    </SafeAreaView>
+  );
+};
+
 const LoginScreen =(navigation)=>{
 
     return (
@@ -198,6 +328,12 @@ const styles = StyleSheet.create({
         color: 'black',
         marginBottom: 20,
         fontWeight: "bold",
+    },
+     item: {
+      padding: 20,
+      marginVertical: 8,
+      marginHorizontal: 16,
+      marginBottom:20
     },
     input: {
         backgroundColor: "#fff",
